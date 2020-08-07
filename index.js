0568b09217854e772c62eca710ae0e14a21d1b02
S1Acharya123 <saketacharya.08@gmail.com>
2020-08-07 14:31:45 +0530

    mofified views files

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // we require express library
 const express = require('express');
-
 const app = express();
-
 const port = 8000;
 
 
 
 
 
+// we will set ejs as our view engine
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
 
+
+// to load static files(css , images , js ,etc) dynamically using middleware
+app.use(express.static('assets'));
+
+
+
+
+
+// code to render page(home.ejs) or simply home page
+app.get('/' , function(req , res){
+    return res.render('home' , {
+        title:"helloooo"
+    });
+})
 
 
 
